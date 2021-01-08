797e67f7b1eb3a72421b5ebc464afa650aa68a99
maxjay <disgracedandbetrayed@gmail.com>
2021-01-08 00:54:44 +0000

    startup

---
+++ startup.lua
@@ -0,0 +1,11 @@
+modem = peripheral.wrap("left")
+
+modem.open(69)
+while true:
+    do
+    local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
+    if message == "Go" then
+        break
+    end
+end
+os.run({}, "rom/MiningTurtle.lua")
