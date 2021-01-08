1689785baf158892794a5abf74244289a8568c38
maxjay <disgracedandbetrayed@gmail.com>
2021-01-08 01:20:22 +0000

    some syntax fixes

--- startup.lua
+++ startup.lua
@@ -4,8 +4,9 @@ modem.open(1)
 while true
     do
     local event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
+    print(message)
     if message == "Go" then
         break
     end
 end
-os.run({}, "rom/MiningTurtle.lua")
+os.run({}, "MiningTurtle.lua")
